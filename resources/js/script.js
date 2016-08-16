108a3c01775b7d3d71c6eb49eef68b4da27b2b19
zackluckyf <fanning.34@gmail.com>
2016-08-15 23:34:56 -0400

    work done

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,48 +1,19 @@
-$(document).ready(function(){
-    
+$(document).ready(function() {
+
     /* Navigation scroll */
-    
+
     $(function() {
-      $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html, body').animate({
-              scrollTop: target.offset().top
-            }, 1000);
-            return false;
-          }
-        }
-      });
-    });
-    
-        /* Mobile Navigation */
-    
-    $('.js--nav-icon').click(function() {
-        var nav = $('.js--main-nav');
-
-        nav.slideToggle(300);
-    });
-    
-    $(window).resize(function(){
-
-        var nav = $('.js--main-nav');
-
-        var icon = $('.js--nav-icon i');
-
-        if ($(window).width() > 699){
-
-        nav.css("display", "block");
-
-        } else {
-
-        nav.css("display", "none");
-
-        icon.addClass('ion-navicon-round');
-            
-        }
-
+        $('a[href*=#]:not([href=#media])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
     });
 });
-
