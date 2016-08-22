53aa64ad9dc9a25d7b966e85b0e807719edc5fdc
zackluckyf <fanning.34@gmail.com>
2016-08-21 21:51:50 -0400

    added working bootstrap gif carousel and fixed smooth scrolling

--- readme.md
+++ readme.md
@@ -3,8 +3,4 @@ zackluckyf.github.io
 
 Current Issues
 
-Carousel
-1 Moves on its own, want to stop that
-2 tic-tac-toe is a dead link (github is empty! push from home computer)
-3 (fixed) next and previous buttons don't work
-4 when clicking on navigation it scrolls too low on the page, want to remove srolling
+Working on the gif Carousel
--- resources/js/script.js
+++ resources/js/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 
-    /* Navigation scroll */
+    "use strict"
+
+    /* Navigation scroll while ignoring bootstrap carousel buttons */
 
     $(function() {
-        $('a[href*=#]:not([href=#media])').click(function() {
+        $('a[href*=\\#]:not([href=\\#myCarousel])').click(function() {
             if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
                 var target = $(this.hash);
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -16,4 +18,21 @@ $(document).ready(function() {
             }
         });
     });
+
+    // gif play on hover and pause on stopping the hover
+
+    $('video').get(0).pause();
+    $('video').get(1).pause();
+    $('video').get(2).pause();
+    $('video').get(3).pause();
+    $('video').get(4).pause();
+    var figure = $(".video").hover(hoverVideo, hideVideo);
+
+    function hoverVideo(e) {
+        $('video', this).get(0).play();
+    }
+
+    function hideVideo(e) {
+        $('video', this).get(0).pause();
+    }
 });
